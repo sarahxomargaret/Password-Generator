6a159529b256b48ecd16cb727e2580643d6760db
Fawn <sarahmargaret@ymail.com>
2023-04-04 17:53:45 -0400

    uploaded correct starter code

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Assignment Code
+// Assignment code here
+
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,14 +13,14 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+
+
+
+  return 
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-const myModal = document.getElementById('exampleModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
 
