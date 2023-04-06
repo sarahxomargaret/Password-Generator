1c54ae160c3fb8b62d201bce1b3e31abed0193cf
Fawn <sarahmargaret@ymail.com>
2023-04-05 21:01:54 -0400

    added popup for including options

--- script.js
+++ script.js
@@ -9,8 +9,39 @@ function generatePassword(){
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var specialChar = "!#%$'()*+,-./:;<=>?@[\]^_`{|}~"
+  var pool = [];
 
 }
+
+// number of characters
+var passwordLength = parseInt(prompt("Choose a password length between 8-128 characters"))
+  if(passwordLength < 8 || passwordLength > 128){
+    prompt("Please choose between 8 and 128");
+  }
+
+//include lowercase letters
+var addLower = confirm("Would you like to add lowercase letters?");
+  if (addLower){
+    pool += lowerCase;
+  }
+
+//include uppercase letters
+var addUpper = confirm("Would you like to add uppercase letters?");
+  if (addUpper){
+    pool +- upperCase
+  }
+
+//include numbers
+var addNumbers = confirm("Would you like to add numbers?");
+  if (addNumbers){
+    pool +- numbers
+  }
+
+//include special characters
+var addCharacters = confirm("Would you like to add special characters?");
+ if (addCharacters){
+  pool +- specialChar
+ }
 
 // Write password to the #password input
 function writePassword() {
@@ -22,6 +53,7 @@ function writePassword() {
 }
 
 function generatePassword() {
+
 
 
 
