2142cef50d2cc57665cd8e40b9384e4ff85361eb
Fawn <sarahmargaret@ymail.com>
2023-04-16 16:47:06 -0400

    fixed issue where it would only generate 1 character

--- script.js
+++ script.js
@@ -6,58 +6,53 @@ function generatePassword(){
   var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   var numbers = ['0','1','2','3','4','5','6','7','8','9'];
   var specialChar = ['!','@','#','$','%','^','&','*','(',')','-','=','+','_','<','>','?'];
-  // var minLength = 8;
-  // var maxLength= 128;
-  var pool = [];
+  var passwordOptions = [];
+  var passwordMinLength = 8;
+  var passwordMaxLength = 128;
   var passwordGenerated = [];
 
 
 // number of characters
-var passwordLength = parseInt(prompt("Choose a password length between 8-128 characters"));
-  if(passwordLength < 8 || passwordLength > 128 === true){
+var passwordLength = prompt("Choose a password length between 8-128 characters");
+  if(passwordLength < passwordMinLength || passwordLength > passwordMaxLength){
     alert("Please choose between 8 and 128");
-    pool = pool.concat(passwordLength);
+    return null;
     // console.log(pool);
   }
 
 //include lowercase letters
 var addLower = confirm("Would you like to add lowercase letters?");
   if (addLower === true){
-    pool = pool.concat(lowerCase);
+    passwordOptions = passwordOptions.concat(lowerCase);
     // console.log(pool);
   }
 
 //include uppercase letters
 var addUpper = confirm("Would you like to add uppercase letters?");
   if (addUpper === true){
-    pool = pool.concat(upperCase);
+    passwordOptions = passwordOptions.concat(upperCase);
     // console.log(pool);
   }
 
 //include numbers
 var addNumbers = confirm("Would you like to add numbers?");
   if (addNumbers === true){
-    pool = pool.concat(numbers);
+    passwordOptions = passwordOptions.concat(numbers);
     // console.log(pool);
   }
 
 //include special characters
 var addCharacters = confirm("Would you like to add special characters?");
  if (addCharacters === true){
-  pool = pool.concat(specialChar);
+  passwordOptions = passwordOptions.concat(specialChar);
   // console.log(pool);
  }
-
- if (!addLower && !addUpper && !addNumbers && !addCharacters){
-  return alert("Must pick at least one");
- }
-
-
-for(var i = 0; i < passwordLength; i++){
-  passwordGenerated = (pool[Math.floor(Math.random() * pool.length)]);
- return passwordGenerated;
+console.log(passwordOptions);
+for(var i = 0; i <passwordLength; i++){
+    passwordGenerated.push(passwordOptions[Math.floor(Math.random() * passwordOptions.length)]);
 }
-
+console.log(passwordOptions);
+return passwordGenerated.join("");
 
 }
 
